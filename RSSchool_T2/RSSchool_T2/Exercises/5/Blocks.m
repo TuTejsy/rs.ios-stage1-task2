24a7b69e703868deffe61f65a8cf3100db00fec6
TuTejsy <1111kaktus1111@gmail.com>
2020-03-20 17:26:42 +0300

    add solutions to tasks 1 and 2

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -2,6 +2,36 @@
 
 @implementation Blocks
 
+- (void) setBlockA: (BlockA) block {
+   // self.blockA = [block copy];
+};
+
+- (BlockA) blockA {
+    return ^(NSArray *array) {
+        NSLog(@"array");
+    };
+}
+
+- (void) setBlockB: (BlockB) block {
+  //  self.blockB = [block copy];
+};
+
+- (BlockB) blockB {
+    return ^(Class class) {
+        NSLog(@"class");
+    };
+}
+
+- (void) setBlockC: (BlockC) block {
+//    __weak weakSelf = self;
+//    weakSelf.blockC = [block copy];
+};
+
+- (BlockC) blockC {
+    return ^(NSObject *result) {
+         NSLog(@"NSObject");
+    };
+}
 
 @end
 
